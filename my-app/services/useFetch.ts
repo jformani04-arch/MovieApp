64dc7ff7d023710a3406d456daa5bdf56e0de0c9
jformani <jformani04@outlook.com>
2025-10-20 18:32:18 -0400

    appWrite backend setup

--- my-app/services/useFetch.ts
+++ my-app/services/useFetch.ts
@@ -1,40 +1,39 @@
-import { useState, useEffect } from 'react'
-
+import { useState, useEffect } from "react";
 
 const useFetch = <T>(fetchFunction: () => Promise<T>, autoFetch = true) => {
-    const [data, setData] = useState<T | null>(null);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState<Error | null>(null);
+  const [data, setData] = useState<T | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<Error | null>(null);
 
-    const fetchData = async () => {
-        try {
-            setLoading(true);
-            setError(null);
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      setError(null);
 
-            const result = await fetchFunction();
-
-            setData(result);
-        } catch (err) {
-            setError(err instanceof Error ? err : new Error('An Error occured'));
-        } finally {
-            setLoading(false);
-        }
+      const result = await fetchFunction();
+      setData(result);
+    } catch (err) {
+      setError(
+        err instanceof Error ? err : new Error("An unknown error occurred")
+      );
+    } finally {
+      setLoading(false);
     }
+  };
 
-    const reset = () => {
-        setData(null);
-        setLoading(false);
-        setError(null);
+  const reset = () => {
+    setData(null);
+    setError(null);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    if (autoFetch) {
+      fetchData();
     }
+  }, []);
 
-    useEffect(() => {
-        if(autoFetch) {
-            fetchData();
-        }
-    }, []);
-
-    return {data, loading, error, refetch: fetchData, reset}
-
-}
+  return { data, loading, error, refetch: fetchData, reset };
+};
 
 export default useFetch;
